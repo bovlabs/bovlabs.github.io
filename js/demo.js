d252402ac39f58edbbda007767b47b75c185e5e5
Jithin Paul <jithin.paul@bovlabs.com>
2018-04-12 23:01:41 +0530

    dynamically shortening text for advisor descr

--- js/demo.js
+++ js/demo.js
@@ -15,12 +15,36 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 // jQuery plugin example:
-$(document).ready(function() {
+$(document).ready(function () {
+  var $tooltip = $('.tooltip');
+  var $tooltipText = $('.tooltiptext');
   $('.particles').particleground({
     dotColor: '#eeeeee',
     lineColor: '#eeeeee'
   });
   $('.intro').css({
     'margin-top': -($('.intro').height() / 2)
+  });
+  $tooltip.each(function (el) {
+    var content = $(this).find('.tooltiptext p').text();
+    if (content.length && content.length > 275) {
+      content = content.substring(0, 270) + '... <span class="show"> show</span>';
+    }
+    $(this).find('.content').html(content);
+  });
+  $tooltip.find('.show').click(function (event) {
+    $tooltipText.css({ "visibility": "hidden", "opacity": 0 });
+    $(this).parent().siblings('.tooltiptext').css({ "visibility": "visible", "opacity": 1 });
+  });
+
+  $(document).click(function (event) {
+    if (!$(event.target).hasClass('show') && !$(event.target).hasClass('descr')) {
+      $tooltipText.css({ "visibility": "hidden", "opacity": 0 });
+    }
+  });
+  $(document).keyup(function (e) {
+    if (e.keyCode == 27) {
+      $tooltipText.css({ "visibility": "hidden", "opacity": 0 });
+    }
   });
 });
